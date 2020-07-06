b028e6cf8b23866c0cbe98d75340f9c4cd13403c
Andrii-Ishchenko <andrey.ischencko@gmail.com>
2020-07-06 18:14:31 +0300

    added IdentityServer4, new project structure

---
+++ src/Together.Auth/Config.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Together.Auth
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("together_api"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "together_webclient",
+                    ClientName = "Angular SPA Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+
+                    RedirectUris =              { "https://localhost:5005/callback" },
+                    PostLogoutRedirectUris =    { "https://localhost:5005/"},
+                    AllowedCorsOrigins =        { "https://localhost:5005" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "together_api"
+                    }
+                }
+
+            };
+    }
+}
--- src/Together.WebApi/Startup.cs
+++ src/Together.WebApi/Startup.cs
@@ -32,24 +32,47 @@ namespace Together.WebApi
         {
             services.AddControllers();
 
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:5001";
+
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("TogetherApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "together_api");
+                });
+            });
+
+            services.AddCors(options => 
+            {
+                options.AddPolicy("default", policy => 
+                {
+                    policy.WithOrigins("https://localhosat:5005")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
+
             services.AddDbContext<TogetherDbContext>(options =>
             {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), opt => {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), opt =>
+                {
                     opt.MigrationsAssembly("Together.DataAccess");
                 });
             });
 
-            services.Configure<IdentityOptions>(options => {
-                options.Password.RequireNonAlphanumeric = false;
-            });
-
-            services.AddIdentity<UserAccount, UserRole>()
-                .AddEntityFrameworkStores<TogetherDbContext>()
-                .AddUserManager<UserAccountManager>()
-                .AddRoleManager<UserRoleManager>();
-
             services.AddTransient<TogetherDBInitializer>();
-                
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,12 +86,14 @@ namespace Together.WebApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("default");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers().RequireAuthorization("ApiScope");
             });
         }
     }
