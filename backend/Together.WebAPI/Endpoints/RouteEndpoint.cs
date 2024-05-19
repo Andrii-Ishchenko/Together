9843b2b2e69c05e0539c8ee61c51a3195e46c1fd
Andrii-Ishchenko <andrey.ischencko@gmail.com>
2024-05-20 00:54:22 +0300

    added route endpoint and refactored program file

---
+++ backend/Together.WebAPI/Endpoints/RouteEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Together.WebAPI.Endpoints;
+
+public static class RouteEndpoint
+{
+    public static IEndpointRouteBuilder MapRouteEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/route/{id}", GetRoute)
+            .WithName("GetRoute");
+
+        return endpoints;
+    }
+
+    private static async Task<IResult> GetRoute(int id)
+    {
+        return await Task.FromResult(Results.Ok($"{id}"));
+    }
+}
---
+++ backend/Together.WebAPI/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Together.WebAPI.Endpoints;
+
+namespace Together.WebAPI.Extensions;
+
+public static class EndpointRouteBuilderExtensions
+{
+    public static IEndpointRouteBuilder MapApplicationEndpoints(this WebApplication endpoints)
+    {
+        endpoints.MapRouteEndpoints();
+
+        return endpoints;
+    }
+}
